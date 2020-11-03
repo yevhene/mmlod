d17b51d62a137c42e368350f88ec1d66e4208df7
Yevhen Shemet <yevhene@gmail.com>
2020-11-03 13:42:36 +0200

    mmlod init commit

---
+++ lib/mmlod.ex
@@ -0,0 +1,18 @@
+defmodule Mmlod do
+  @moduledoc """
+  Documentation for `Mmlod`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mmlod.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mmlod_test.exs
@@ -0,0 +1,8 @@
+defmodule MmlodTest do
+  use ExUnit.Case
+  doctest Mmlod
+
+  test "greets the world" do
+    assert Mmlod.hello() == :world
+  end
+end
